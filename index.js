0b68c9ddc3db803b8981d60342c670f74c9e2a55
egwuenu gift <laurandidi21@gmail.com>
2017-10-09 00:42:40 +0100

    refactored  code :fire:

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.set("view engine", "ejs");
 
 var campgrounds = [
     // arrays of different campgrounds and images
-    {name: "SalmonCreek", image:"https://images.unsplash.com/photo-1487730116645-74489c95b41b?dpr=1&auto=compress,format&fit=crop&w=750&h=&q=80&cs=tinysrgb&crop="},
     {name: "MountBriton", image:"https://images.unsplash.com/photo-1445308394109-4ec2920981b1?dpr=1&auto=compress,format&fit=crop&w=753&h=&q=80&cs=tinysrgb&crop="},
-    {name: "AgbejuleRock", image:"https://images.unsplash.com/photo-1470246973918-29a93221c455?dpr=1&auto=compress,format&fit=crop&w=334&h=&q=80&cs=tinysrgb&crop="},
-    {name: "IyanuMasheke", image:"https://images.unsplash.com/photo-1504851149312-7a075b496cc7?dpr=1&auto=compress,format&fit=crop&w=349&h=&q=80&cs=tinysrgb&crop="},
-    {name: "LaunrynHill", image:"https://images.unsplash.com/photo-1455763916899-e8b50eca9967?dpr=1&auto=compress,format&fit=crop&w=750&h=&q=80&cs=tinysrgb&crop="}      
+    {name: "SalmonCreek", image:"https://images.unsplash.com/photo-1487730116645-74489c95b41b?dpr=1&auto=compress,format&fit=crop&w=750&h=&q=80&cs=tinysrgb&crop="},
+    {name: "oifyefofwrg", image:"https://images.unsplash.com/photo-1455763916899-e8b50eca9967?dpr=1&auto=compress,format&fit=crop&w=750&h=&q=80&cs=tinysrgb&crop="},    
+    {name: "Launrwrgrgg", image:"https://images.unsplash.com/photo-1455763916899-e8b50eca9967?dpr=1&auto=compress,format&fit=crop&w=750&h=&q=80&cs=tinysrgb&crop="},     
+    {name: "Lauwggrgq", image:"https://images.unsplash.com/photo-1455763916899-e8b50eca9967?dpr=1&auto=compress,format&fit=crop&w=750&h=&q=80&cs=tinysrgb&crop="}      
 ]
 
 //Route for landing page
@@ -21,14 +21,7 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        // arrays of different campgrounds and images
-        {name: "SalmonCreek", image:"https://images.unsplash.com/photo-1487730116645-74489c95b41b?dpr=1&auto=compress,format&fit=crop&w=750&h=&q=80&cs=tinysrgb&crop="},
-        {name: "MountBriton", image:"https://images.unsplash.com/photo-1445308394109-4ec2920981b1?dpr=1&auto=compress,format&fit=crop&w=753&h=&q=80&cs=tinysrgb&crop="},
-        {name: "AgbejuleRock", image:"https://images.unsplash.com/photo-1470246973918-29a93221c455?dpr=1&auto=compress,format&fit=crop&w=334&h=&q=80&cs=tinysrgb&crop="},
-        {name: "IyanuMasheke", image:"https://images.unsplash.com/photo-1504851149312-7a075b496cc7?dpr=1&auto=compress,format&fit=crop&w=349&h=&q=80&cs=tinysrgb&crop="},
-        {name: "LaunrynHill", image:"https://images.unsplash.com/photo-1455763916899-e8b50eca9967?dpr=1&auto=compress,format&fit=crop&w=750&h=&q=80&cs=tinysrgb&crop="}      
-    ]
+    
     res.render("campgrounds", {campgrounds: campgrounds});
     
 });
